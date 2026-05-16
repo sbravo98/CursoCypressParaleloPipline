0d8599f1ab3050ddb1fb74e83173889068d19522
steeven <steeven.bravo98@gmail.com>
2026-05-15 20:28:04 -0500

    Fix Cypress reference

--- cypress/e2e/seccion_1/hola_mund.cy copy 2.js
+++ cypress/e2e/seccion_1/hola_mund.cy copy 2.js
@@ -1,15 +1,23 @@
-//// <refrence types="cypress" />
+/// <reference types="cypress" />
 
-describe('Primer test  con cypress', () => {
+describe('Primer test con cypress', () => {
+
     it('Este es un hola mundo de Cypress', () => {
+
         cy.log("Bienvenidos a Cypress");
-        cy.visit("https://ultimateqa.com/complicated-page/")
+
+        cy.visit("https://ultimateqa.com/complicated-page/");
 
         cy.get('input[name="log"]').first().type("Steeven");
+
         cy.get('input[name="pwd"]').first().type("password123");
 
-        cy.get('button[name="et_builder_submit_button"]').first().click();
-        cy.wait(3000)
-        
-    })
-    })
+        cy.get('button[name="et_builder_submit_button"]')
+            .first()
+            .click();
+
+        cy.wait(3000);
+
+    });
+
+});
