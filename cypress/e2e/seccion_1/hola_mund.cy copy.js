49fc25eed9961d2ca1e2c68811d484ac47244023
steeven <steeven.bravo98@gmail.com>
2026-05-15 20:02:21 -0500

    Inicio de proyecto

---
+++ cypress/e2e/seccion_1/hola_mund.cy copy.js
@@ -0,0 +1,15 @@
+//// <refrence types="cypress" />
+
+describe('Primer test  con cypress', () => {
+    it('Este es un hola mundo de Cypress', () => {
+        cy.log("Bienvenidos a Cypress");
+        cy.visit("https://ultimateqa.com/complicated-page/")
+
+        cy.get('input[name="log"]').first().type("Steeven");
+        cy.get('input[name="pwd"]').first().type("password123");
+
+        cy.get('button[name="et_builder_submit_button"]').first().click();
+        cy.wait(3000)
+        
+    })
+    })
