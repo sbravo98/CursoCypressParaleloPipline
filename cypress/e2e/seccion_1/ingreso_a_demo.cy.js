97d49b811e65312290b77c01b788d7a67807b972
steeven <steeven.bravo98@gmail.com>
2026-05-15 21:29:28 -0500

    Add Mikareno Cypress tests

--- cypress/e2e/seccion_1/ingreso_a_demo.cy.js
+++ cypress/e2e/seccion_1/ingreso_a_demo.cy.js
@@ -5,20 +5,6 @@ import 'cypress-file-upload';
 
 describe (" Elementos de una tabla ",() => {
 
-    it(" Children ", () => {
-        cy.viewport(1920,1080);
-        cy.visit("https://demo.mikareno.com/login.action");
-        cy.title().should('eq', 'Mikareno');
-        cy.get('#username').should('be.visible').type("celeste.maldonado@demo.com");
-        cy.get('#password').should('be.visible').type("mikareno");
-        cy.xpath('//*[@id="loginForm"]/div[2]/button').should('be.visible').click();
-        cy.get('.main-account').should('be.visible').click();
-        cy.visit("https://demo.mikareno.com/persons/list-persons.action");
-        cy.get('.pagination').contains('.paginate_button.page-item', '2').click({ force: true })
-        cy.wait(2000);
-        cy.get('.pagination').contains('.paginate_button.page-item', '3').click({ force: true })
-        })
-
     it(" Mostrando los campos ", () => {
         cy.viewport(1920,1080);
         cy.visit("https://demo.mikareno.com/login.action");
@@ -27,7 +13,6 @@ describe (" Elementos de una tabla ",() => {
         cy.get('#password').should('be.visible').type("mikareno");
         cy.xpath('//*[@id="loginForm"]/div[2]/button').should('be.visible').click();
         cy.get('.main-account').should('be.visible').click();
-        cy.visit("https://demo.mikareno.com/persons/list-students.action");
 
         })
 
